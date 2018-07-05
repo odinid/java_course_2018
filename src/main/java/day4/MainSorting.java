f6d4aeff378847cf044d124e401a5c33c9815b3d
Piyarach Saeoui <odinsci@gmail.com>
2018-07-05 13:15:40 +0700

    Day4 Sorting

--- src/main/java/day4/MainSorting.java
+++ src/main/java/day4/MainSorting.java
@@ -14,17 +14,28 @@ public class MainSorting {
 
     private void start() {
         // Create Data
+        List<Employee> employees = initialData();
+        employees.forEach(System.out::println);
+        System.out.println();        
+
+        // Sorting with Comparator
+        Collections.sort(employees, new SortingBySalaryWithDecending());
+        
+        // Sorting with Lambda
+        Comparator<Employee> sortingBySalaryWithLambda = 
+                (Employee e1, Employee e2) -> (int)(e2.getSalary() - e1.getSalary());
+        Collections.sort(employees, sortingBySalaryWithLambda);
+                    
+        employees.forEach(System.out::println);
+    }
+
+    private List<Employee> initialData() {
         List<Employee> employees = new ArrayList<>();
         employees.add(new Employee(1, "Piyarach", 200));
         employees.add(new Employee(2, "Pui", 100));
         employees.add(new Employee(3, "Abdul", 500));
         employees.add(new Employee(4, "Up1", 150));
-        
-        // Sorting with Comparator
-        Collections.sort(employees, new SortingBySalaryWithDecending());
-        
-        employees.forEach(System.out::println);
-
+        return employees;
     }
 }
 
@@ -33,7 +44,6 @@ class SortingBySalaryWithDecending implements Comparator<Employee> {
     public int compare(Employee e1, Employee e2) {
         // -1 = DESC, 0 = Not change, 1 = ASC
         //return e1.getName().compareTo(e2.getName());
-        //return (int)(e1.getSalary() - e2.getSalary());
-        return (e2.getSalary() - e1.getSalary() > 0 ? -1 : 1 );
+        return (int)(e2.getSalary() - e1.getSalary());
     }
 }
---
+++ src/main/java/day4/MainStream.java
@@ -0,0 +1,47 @@
+package day4;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class MainStream {
+
+    public static void main(String[] args) {
+        MainStream p = new MainStream();
+        p.start();
+    }
+
+    private void start() {
+        // Array
+        String[] datas = new String[] {"One", "Two", "Three"};
+        Stream<String> stream = Arrays.stream(datas);
+        stream.forEach(System.out::println);
+        
+        // Collection
+        List<String> lists = new ArrayList<>();
+        lists.add("One");
+        lists.add("Two");
+        lists.add("Three");
+        lists.stream().forEach(System.out::println);
+        
+        System.out.println();
+        
+        // Filter data that start with "T"
+        List<String> after = new ArrayList<>();
+        for (String string : lists) {
+            if (string.startsWith("T")) {
+                after.add(string);
+            }
+        }
+        after.forEach(System.out::println);
+        
+        System.out.println();
+        
+        lists.stream()
+        .filter(string -> string.startsWith("T"))
+        .map(string -> string.toLowerCase())
+        .forEach(System.out::println);
+    }
+
+}
